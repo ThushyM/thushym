3c094f499c237a6e1fa385e7cb4a8ba191432d6a
Klarence <ouyang.klarence@gmail.com>
2024-05-09 09:51:39 -0500

    updated design

--- js/plugins.js
+++ js/plugins.js
@@ -1,15 +1,16 @@
 // Avoid `console` errors in browsers that lack a console.
-(function() {
-  var method;
-  var noop = function () {};
-  var methods = [
+(function () {
+  let method;
+  const noop = function () {
+  };
+  const methods = [
     'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
     'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
     'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
     'timeline', 'timelineEnd', 'timeStamp', 'trace', 'warn'
   ];
-  var length = methods.length;
-  var console = (window.console = window.console || {});
+  let length = methods.length;
+  const console = (window.console = window.console || {});
 
   while (length--) {
     method = methods[length];
@@ -22,3 +23,13 @@
 }());
 
 // Place any jQuery/helper plugins in here.
+const navToggle = document.querySelector('#side-menu')
+const navLink = document.querySelectorAll('.nav-item');
+
+for (let i = 0; i < navLink.length; ++i) {
+  navLink[i].addEventListener('click', (event) => {
+    if (event.target.classList.contains('nav-item')) {
+      navToggle.checked = false;
+    }
+  });
+}
